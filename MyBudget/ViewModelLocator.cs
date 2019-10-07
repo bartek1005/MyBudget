100a5807dbee6b3532f63381c65fe9863be59708
Bartek Kawik <bartek.kawik@gmail.com>
2019-10-06 22:00:09 -0400

    UserControls and AutoWiring

--- MyBudget/ViewModelLocator.cs
+++ MyBudget/ViewModelLocator.cs
@@ -1,29 +1,58 @@
 ﻿using MyBudget.ViewModel;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace MyBudget
 {
-    public class ViewModelLocator
+    public static class ViewModelLocator
     {
-        private static ExpenseListViewModel _expenseListViewModel = new ExpenseListViewModel();
-        private static ExpenseChartsViewModel expenseChartsViewModel = new ExpenseChartsViewModel();
-        public static ExpenseListViewModel ExpenseListViewModel
+        //private static ExpenseListViewModel _expenseListViewModel = new ExpenseListViewModel();
+        //private static ExpenseChartsViewModel expenseChartsViewModel = new ExpenseChartsViewModel();
+        //public static ExpenseListViewModel ExpenseListViewModel
+        //{
+        //    get
+        //    {
+        //        return _expenseListViewModel;
+        //    }
+        //}
+        //public static ExpenseChartsViewModel ExpenseChartsViewModel
+        //{
+        //    get
+        //    {
+        //        return expenseChartsViewModel;
+        //    }
+        //}
+
+
+
+        public static bool GetAutoWireViewModel(DependencyObject obj)
         {
-            get
-            {
-                return _expenseListViewModel;
-            }
+            return (bool)obj.GetValue(AutoWireViewModelProperty);
         }
-        public static ExpenseChartsViewModel ExpenseChartsViewModel
+
+        public static void SetAutoWireViewModel(DependencyObject obj, bool value)
         {
-            get
-            {
-                return expenseChartsViewModel;
-            }
+            obj.SetValue(AutoWireViewModelProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AutoWireViewModelProperty =
+            DependencyProperty.RegisterAttached("AutoWireViewModel", typeof(bool), typeof(ViewModelLocator), new PropertyMetadata(false,AutoWireViewModelChanged));
+
+        private static void AutoWireViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (DesignerProperties.GetIsInDesignMode(d)) return;
+            var viewType = d.GetType();
+            var viewTypeName = viewType.FullName;
+            var viewModelTypeName = viewTypeName.Replace("View", "ViewModel");
+            var viewModelType = Type.GetType(viewModelTypeName);
+            var viewModel = Activator.CreateInstance(viewModelType);
+            ((FrameworkElement)d).DataContext = viewModel;
         }
     }
 }
