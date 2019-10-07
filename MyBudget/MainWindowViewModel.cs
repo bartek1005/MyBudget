465937a1203b7413fe75d3a38ba4a33bac5e32a5
Bartek Kawik <bartek.kawik@gmail.com>
2019-10-06 22:27:35 -0400

    MainWindowViewModel

---
+++ MyBudget/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MyBudget.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyBudget
+{
+    public class MainWindowViewModel
+    {
+        public object CurrentViewModel { get; set; }
+        public MainWindowViewModel()
+        {
+            CurrentViewModel = new ExpenseAllViewModel();
+        }
+    }
+}
